50adf81aca7ae559252e22a34a82a2ebee1f3f58
nchung <nchung.dev@gmail.com>
2019-04-29 09:25:15 +0700

    Update
    Signed-off-by: nchung <nchung.dev@gmail.com>

--- NeTodo/app/src/main/java/com/nchung/netodo/ui/add_todo/CalendarBottomSheet.kt
+++
@@ -1,8 +0,0 @@
-package com.nchung.netodo.ui.add_todo
-
-import com.nchung.netodo.R
-import com.nchung.netodo.ui.base.BaseBottomSheet
-
-class CalendarBottomSheet : BaseBottomSheet() {
-    override fun getLayoutId() = R.layout.layout_calendar_view
-}
--- NeTodo/app/src/main/java/com/nchung/netodo/ui/drawer_menu/DrawerMenuItem.kt
+++
@@ -1,6 +0,0 @@
-package com.nchung.netodo.ui.drawer_menu
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-class DrawerMenuItem(@DrawableRes var iconId: Int, @StringRes var titleId: Int,var badge: Int = 0)
